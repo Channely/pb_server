4273e1ffe81075962d88e4dd1baab121335683e6
Channely <qijie29896@gmail.com>
2013-12-05 10:10:52 +0800

    initial project for party-bid

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PbServer::Application.config.secret_key_base = '26619ea20912ff97cc14cdd72d6a1f1e71b19a47efc44348858b284856f46fb5a254b9b464103ae6eb8e5020f39b1f769a07502c7b14103db974f825f9447872'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PbServer::Application.config.session_store :cookie_store, key: '_pb_server_session'
