502663aac355a81bd1b494a1b2e45257d93a9e7e
Channely <qijie29896@gmail.com>
2013-12-11 19:55:15 +0800

    ccy - hcange db elements attr

--- db/schema.rb
+++ db/schema.rb
@@ -14,11 +14,11 @@
 ActiveRecord::Schema.define(version: 20131210073355) do
 
   create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password"
-    t.string   "question"
-    t.string   "answer"
+    t.text     "name"
+    t.text     "email"
+    t.text     "password"
+    t.text     "question"
+    t.text     "answer"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "remember_token"
