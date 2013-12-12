a38e48d93b5e0ab1cd453ef144c95611bc03743d
Channely <qijie29896@gmail.com>
2013-12-12 15:20:01 +0800

    ccy - fix the bug of showing success info as alert style, nad can dispaly normal

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,11 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user[:password]==params[:session][:password]
       sign_in user
+      flash[:success] = "Welcome "+user.name+", pleasure for your using!"
+                             #"欢迎尊敬的"+@user.name+"来到活动通，祝您使用愉快。"
       redirect_back_or user
     else
-      flash.now[:error] = 'Invalid email/password combination.'
+      flash.now[:danger] = 'Invalid email/password combination.'
       render 'new'
     end
   end
