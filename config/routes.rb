fdbe267208728667a0c850559bb837c650de57d0
Channely <qijie29896@gmail.com>
2013-12-12 15:22:29 +0800

    ccy - add user-profile page in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ PbServer::Application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
 
   root 'navigates#home'
+  match "help",     to: 'navigates#help',   via: 'get'
 
+  match "add_user", to: 'users#new',        via: 'get'
   match "signup",   to: 'users#new',        via: 'get'
   match "signin",   to: 'sessions#new',     via: 'get'
   match "signout",  to: 'sessions#destroy', via: 'delete'
